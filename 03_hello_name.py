ebdde5c7b5c3516d36e42a188f1a726ac9703ce3
MS1 <157831131+coderms1@users.noreply.github.com>
2025-11-14 11:47:13 -0500

    Add a greeting program with user input

---
+++ 03_hello_name.py
@@ -0,0 +1,20 @@
+# Using Variables in Print Statements
+
+# Assignment & Prompt (input) --> Print Results (output)!
+name = "Johnny"
+age = 50
+print(f"Hello, my name is {name} and I am {age} years old.")
+
+user_name1 = input("What is your name? ")
+print(f"Hello, {user_name1}! Welcome to Python!")
+
+print("This is my first Python program!")
+nickname = input("Enter your nickname: ")
+user_age = int(input("Enter age: "))
+color = input("Enter favorite color: ")
+
+print(f"Nice to meet you, {name}!")
+print(f"You're, {age} years old and your favorite color is {color}.")
+
+year_when_100 = (2025 + (100 - user_age))
+print(f"You will turn 100 year old in the year, {year_when_100}")
