35a8bbf9ef1395ff6d00856447324bcd22014d32
MS1 <157831131+coderms1@users.noreply.github.com>
2025-11-14 11:45:31 -0500

    Add variable declarations and print statement

---
+++ 02_variables.py
@@ -0,0 +1,26 @@
+# Variables: Data Types
+
+# Strings
+name = "Matthew"
+
+# Integers
+age = 36
+
+# Floats
+gpa = 3.9
+
+# Booleans
+isEnrolled = True
+
+# Lists - mutable (ABLE TO BE CHANGED) 
+hobbies = ["guitar", "coding", "snowboarding", "tennis", "billiards", "music"]
+
+# Dictionaries
+pets = {"cat": "Moose"}
+
+# Tuples - immmuatable (UNABLE TO BE CHANGED)
+fam_members = ["Bob", "Rose", "Trey"]
+
+print(f"Greetings, my name is {name} and I am {age} years old.\
+I have hobbies including: {hobbies}. I attend school and my gpa is: {gpa}.\
+I have 3 people in my family, their names are {fam_members}.")
