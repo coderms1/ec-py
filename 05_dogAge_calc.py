12c5fae96c8986858a2b4734edc217b8f4215896
MS1 <157831131+coderms1@users.noreply.github.com>
2025-11-14 11:48:47 -0500

    Add dog age calculator and weight constants

---
+++ 05_dogAge_calc.py
@@ -0,0 +1,49 @@
+#Dog Age Calculator
+#Constants
+DOGAGE = 7.3
+
+#Input
+sName = input("What is your dog's name: ")
+fAge = float(input(f"What is {sName}'s age: "))
+
+#Calculate
+fHumanAge = fAge * DOGAGE
+
+#Output
+
+print(f"{sName}'s human age is . {fHumanAge}")
+#####
+name1 = "Gordan"
+name2 = "Renee"
+name3 = "Johnny"
+name4 = "Lucy"
+
+print(f"***{name1:^20}***")
+print(f"***{name2:^20}***")
+print(f"***{name3:^20}***")
+print(f"***{name4:^20}***")
+
+########
+num1 = 127.899
+num2 = 3465.148
+num3 = 3.776
+num4 = 264.821
+num5 = 88.081
+num6 = 799.999
+
+#Each number is displayed in a field of 20 space
+#rounded to decimal place
+
+print(f"{num1:10,.2f}{num2:20,.2f}")
+print(f"{num3:10,.2f}{num4:20,.2f}")
+print(f"{num5:10,.2f}{num6:20,.2f}")
+
+
+#########
+#Constants average weights in kg
+LAPTOP_WEIGHT = 2.5     # average laptop
+DOG_WEIGHT = 10.0       # small dog
+HUMAN_WEIGHT = 70.0     # average human
+MOTORBIKE_WEIGHT = 180.0  #average motorbike
+CAR_WEIGHT = 1500.0     # small car
+ELEPHANT_WEIGHT = 5000.0  # adult elephant
